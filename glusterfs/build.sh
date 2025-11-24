c8667527d439ee9b070232f640414bd0de97d1c8
smartmeterdev <dev@smartmeter.tec.br>
2025-11-24 15:45:00 -0300

    build

--- glusterfs/build.sh
+++ glusterfs/build.sh
@@ -3,3 +3,6 @@ docker plugin set glusterfs SERVERS=Pro-de-Manoel.lan
 docker plugin enable glusterfs:latest 
 
 docker volume create --name portrainer_data -d glusterfs
+
+docker cp ../nginx.conf portrainer_data:/etc/nginx/nginx.conf
+docker cp ../conf.d portrainer_data:/etc/nginx/conf.d
--- nginx.dockerfile
+++
@@ -1,5 +0,0 @@
-FROM nginxproxy/nginx-proxy:1.9
-COPY ./nginx.conf /etc/nginx/nginx.conf
-COPY ./conf.d /etc/nginx/conf.d
-
-
