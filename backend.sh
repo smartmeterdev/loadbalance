7968674dce425bc868d1f4ae132094ea688ac9d1
smartmeterdev <dev@smartmeter.tec.br>
2025-11-25 09:41:12 -0300

    fix

--- backend.sh
+++ backend.sh
@@ -1,3 +1,4 @@
+docker config create nginx_conf etc/nginx/nginx.conf
 docker config create nginx_default_conf etc/nginx/conf.d/default.conf
 docker config create nginx_moodle_conf etc/nginx/conf.d/moodle.conf
 docker config create nginx_portainer_conf etc/nginx/conf.d/portainer.conf
