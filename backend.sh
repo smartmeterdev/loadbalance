c0d980ec8fea578ce6957f018557cf82047e9854
smartmeterdev <dev@smartmeter.tec.br>
2025-11-25 09:30:21 -0300

    bug

--- backend.sh
+++ backend.sh
@@ -1,7 +1,7 @@
-docker config nginx_default_conf etc/nginx/conf.d/default.conf
-docker config nginx_moodle_conf etc/nginx/conf.d/moodle.conf
-docker config nginx_portainer_conf etc/nginx/conf.d/portainer.conf
-docker proxysql_cnf etc/proxysql.cnf
+docker config create nginx_default_conf etc/nginx/conf.d/default.conf
+docker config create nginx_moodle_conf etc/nginx/conf.d/moodle.conf
+docker config create nginx_portainer_conf etc/nginx/conf.d/portainer.conf
+docker config create proxysql_cnf etc/proxysql.cnf
 
 
 docker stack deploy -c backend.yml backend
