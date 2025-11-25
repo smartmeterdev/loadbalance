92425919faaee9c6e7048a7654f2fb96ae6bea83
smartmeterdev <dev@smartmeter.tec.br>
2025-11-25 10:25:04 -0300

    configs

--- backend.sh
+++ backend.sh
@@ -1,8 +1,8 @@
-docker config create nginx_conf etc/nginx/nginx.conf
-docker config create nginx_default_conf etc/nginx/conf.d/default.conf
-docker config create nginx_moodle_conf etc/nginx/conf.d/moodle.conf
-docker config create nginx_portainer_conf etc/nginx/conf.d/portainer.conf
-docker config create proxysql_cnf etc/proxysql/proxysql.cnf
+#docker config create nginx_conf etc/nginx/nginx.conf
+#docker config create nginx_default_conf etc/nginx/conf.d/default.conf
+#docker config create nginx_moodle_conf etc/nginx/conf.d/moodle.conf
+#docker config create nginx_portainer_conf etc/nginx/conf.d/portainer.conf
+#docker config create proxysql_cnf etc/proxysql/proxysql.cnf
 
 
 docker stack deploy -c backend.yml backend
